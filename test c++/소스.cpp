0911072ad1466aa369599a473cbe47a3bd55eba2
Shin jihoon <tlswl7479@gmail.com>
2022-03-20 23:24:29 +0900

    초기 커밋.

---
+++ test c++/소스.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	int n1, n2, n3;
+
+	cin >> n1 >> n2 >> n3;
+	if (n1 < n2)
+	{
+		if (n2 < n3)
+		{
+			cout << n3;
+		}
+		else
+		{
+			cout << n2;
+		}
+	}
+	else
+	{
+		if (n3 < n1)
+		{
+			if (n1 < n2) {
+				cout << n2;
+			}
+			else
+			{
+				cout << n1;
+			}
+		}
+	}
+	return 0;
+	*/
+
+	/*
+	double x[10], y[10];
+	int i;
+	double max, area;
+
+	for (i = 0; i < 10; i++)
+	{
+		cin >> x[i] >> y[i];
+	}
+
+	max = 0;
+	for (i = 0; i < 10; i++)
+	{
+		area = x[i] * y[i];
+		if (area < 0) area = -area;
+		if (max < area)max = area;
+	}
+
+	cout << max;
+	*/
+
+
+
+
+}
